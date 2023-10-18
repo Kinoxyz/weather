3702be1dd72c4e3f2776929f274635d9835fca4f
Daniel Agyapong <kunkumah@gmail.com>
2023-10-18 14:22:21 +0200

    Create WeatherCard component

---
+++ src/components/WeatherCard.svelte
@@ -0,0 +1,14 @@
+<div class="card">
+  <slot />
+</div>
+
+<style>
+  .card {
+    background: fixed;
+    padding: 20px;
+    border-radius: 6px;
+    width: max-content;
+
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  }
+</style>
