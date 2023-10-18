e2add2926016af1dbd759062dbddfc2cafacaba7
Daniel Agyapong <kunkumah@gmail.com>
2023-10-18 14:22:07 +0200

    Update LocationInputField

---
+++ src/components/LocationInputField.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/tauri";
+
+  let data = {};
+  let location = "";
+
+  async function fetchWeatherData() {
+    data = await invoke("get_weather_data", { location });
+    console.log(data);
+  }
+</script>
+
+<div>
+  <form class="row" on:submit|preventDefault={fetchWeatherData}>
+    <input
+      id="location-input"
+      placeholder="Enter a location..."
+      bind:value={location}
+    />
+    <button type="submit">Submit</button>
+  </form>
+</div>
