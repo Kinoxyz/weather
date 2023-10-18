cf86cf0b9eabb3c00a1bd6b24197757fa993e77b
Daniel Agyapong <kunkumah@gmail.com>
2023-10-18 17:13:28 +0200

    create basic weather widgets

---
+++ src/components/widgets/WeatherConditionWidget.svelte
@@ -0,0 +1,14 @@
+<div class="card">
+  <slot />
+</div>
+
+<style>
+  .card {
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 10px;
+    margin: 10px;
+    background-color: #f9f9f9;
+    width: 150px;
+  }
+</style>
