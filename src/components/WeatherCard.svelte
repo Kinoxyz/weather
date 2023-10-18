4e431f0ab8b5b3544990f9bd106f28758fe4c4d6
Daniel Agyapong <kunkumah@gmail.com>
2023-10-18 17:13:43 +0200

    Refactor weathercard

--- src/components/WeatherCard.svelte
+++ src/components/WeatherCard.svelte
@@ -1,14 +1,64 @@
+<script>
+  export let data; // Weather data
+  let widgets = [1, 2, 3]; // Widgets for the top row
+  let largeWidget = 4; // Large widget for the bottom
+</script>
+
 <div class="card">
-  <slot />
+  <div class="grid">
+    {#each widgets as widget (widget)}
+      <div class="widget">
+        <!-- Create widgets here -->
+      </div>
+    {/each}
+  </div>
+  <div class="large-widget">
+    <p>
+      Temperature at 00:00 : {JSON.stringify(data.hourly.temperature_2m[0])}
+    </p>
+  </div>
 </div>
 
 <style>
   .card {
-    background: fixed;
     padding: 20px;
     border-radius: 6px;
-    width: max-content;
-
+    width: 600px;
+    height: 400px;
+    min-height: 400px;
     box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+    margin-top: 20px;
+    margin-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 10px;
+    flex: 1;
+  }
+
+  .widget {
+    background-color: #3498db;
+    border: 1px solid #2979ac;
+    border-radius: 5px;
+    padding: 10px;
+    text-align: center;
+    color: #fff;
+    font-weight: bold;
+    margin-bottom: 5px;
+  }
+
+  .large-widget {
+    background-color: #3498db;
+    border: 1px solid #2979ac;
+    border-radius: 5px;
+    padding: 10px;
+    text-align: center;
+    color: #fff;
+    font-weight: bold;
+    flex: 1;
   }
 </style>
