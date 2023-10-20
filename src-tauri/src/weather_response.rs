0b114d61696ab743be8cc511b1c4b1e0f7184284
Daniel Agyapong <kunkumah@gmail.com>
2023-10-20 14:29:55 +0200

    add current weather struct

--- src-tauri/src/weather_response.rs
+++ src-tauri/src/weather_response.rs
@@ -26,3 +26,38 @@ pub struct HourlyUnits {
     #[serde(rename = "temperature_2m")]
     temperature_2_m: String,
 }
+
+#[derive(Serialize, Deserialize)]
+pub struct CurrentWeatherResponse {
+    latitude: f64,
+    longitude: f64,
+    generationtime_ms: f64,
+    utc_offset_seconds: i64,
+    timezone: String,
+    timezone_abbreviation: String,
+    elevation: f64,
+    current_units: CurrentUnits,
+    current: Current
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CurrentUnits {
+    time: String,
+    interval: String,
+    #[serde(rename = "temperature_2m")]
+    temperature_2_m: String,
+    #[serde(rename = "windspeed_2m")]
+    windspeed_2_m: String,
+    weathercode: String
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Current {
+    time: String,
+    interval: f64,
+    #[serde(rename = "temperature_2m")]
+    temperature_2_m: f64,
+    #[serde(rename = "windspeed_2m")]
+    windspeed_2_m: f64,
+    weathercode: f64
+}
