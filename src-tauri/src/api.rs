82c69f4544b181a5e93ff7347765c43e74db37f2
Daniel Agyapong <kunkumah@gmail.com>
2023-10-20 14:34:27 +0200

    update api

--- src-tauri/src/api.rs
+++ src-tauri/src/api.rs
@@ -1,7 +1,7 @@
 use crate::weather_response::ForecastWeatherResponse;
 
 const API_URL: &str =
-    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m";
+    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,windspeed_10m,weathercode&timezone=Europe%2FBerlin";
 
 pub async fn fetch_basic_weather_data() -> Result<ForecastWeatherResponse, Box<dyn std::error::Error>> {
     let response = reqwest::get(API_URL)
