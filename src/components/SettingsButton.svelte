229c2bb0fe6cea439e43cb1147556fce045c078f
Kenny Ketelaar <kenny@ketelaar.tech>
2023-11-14 17:33:23 +0100

    Added settings menu with changing temperature

--- src/components/SettingsButton.svelte
+++ src/components/SettingsButton.svelte
@@ -1,27 +1,55 @@
 <script>
-  import { Temperature } from "../models/Temperature";
-  import { temperatureUnit } from "../stores";
+	import { Temperature } from '../models/Temperature';
+	import Modal from './Modal.svelte';
+	import { temperatureUnit } from '../stores';
+
+	let unit = $temperatureUnit;
+
+	function saveSettings() {
+		temperatureUnit.set(unit)
+	}
+
+	let showModal = false;
+
 </script>
 
-<div
-  on:click={() => temperatureUnit.set(Temperature.Fahrenheit)}
-  on:dblclick={() => temperatureUnit.set(Temperature.Celsius)}
-  class="settings-container"
+<button
+	on:click={() => (showModal = true)}
+	class='settings-container'
 >
-  <img src="/settings_material.svg" alt="Settings icon" />
-</div>
+	<img src='/settings_material.svg' alt='Settings icon' />
+</button>
+
+<Modal closeButtonText='Save settings' bind:showModal onCloseFunction={saveSettings}>
+	<h2 slot='header'>
+		Settings
+	</h2>
+
+	Temperature:
+	<label>
+		<input type='radio' bind:group={unit} value={Temperature.Celsius} />
+		Celsius
+	</label>
+	<label>
+		<input type='radio' bind:group={unit} value={Temperature.Fahrenheit} />
+		Fahrenheit
+	</label>
+</Modal>
 
 <style>
-  .settings-container {
-    display: flex;
-    align-self: flex-end;
-    padding: 1em;
-    padding-bottom: 0%;
-  }
+    /* filter from https://isotropic.co/tool/hex-color-to-css-filter/ */
+    img {
+        filter: invert(100%) sepia(100%) saturate(2%) hue-rotate(26deg) brightness(106%) contrast(101%);
+    }
 
-  /* filter from https://isotropic.co/tool/hex-color-to-css-filter/ */
-  img {
-    filter: invert(100%) sepia(100%) saturate(2%) hue-rotate(26deg)
-      brightness(106%) contrast(101%);
-  }
+    button {
+        margin-right: 1em;
+        align-self: flex-end;
+        width: fit-content;
+    }
+
+    button img {
+        vertical-align: middle;
+    }
+
 </style>
