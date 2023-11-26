589a60d0cffeca089d28632b4d3818e9ae6e0ae0
M.D.V. Meijer <mdvmeijer@protonmail.com>
2023-11-26 20:54:24 +0100

    Use const instead of let in Svelte store

--- src/stores.ts
+++ src/stores.ts
@@ -2,5 +2,5 @@ import {writable} from "svelte/store";
 import {Temperature} from "./models/Temperature";
 
 export const temperatureUnit = writable(Temperature.Celsius)
-export let currentLocationName = writable("");
-export let currentLocationCountry = writable("");
+export const currentLocationName = writable("");
+export const currentLocationCountry = writable("");
